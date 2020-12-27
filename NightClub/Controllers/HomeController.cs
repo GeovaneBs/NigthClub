685ffdecb349e5b86aac1ac30c9765715cbc60bd
Geovane Silva <geovaneb.gb@hotmail.com>
2020-12-27 11:26:22 -0300

    Criada a pagina de Vendas

--- NightClub/Controllers/HomeController.cs
+++ NightClub/Controllers/HomeController.cs
@@ -29,6 +29,10 @@ namespace NightClub.Controllers
         {
             return View();
         }
+        public IActionResult Sale()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
